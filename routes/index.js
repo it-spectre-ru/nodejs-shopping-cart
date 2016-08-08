5b3deac4bb82aaa90176ae3b19bf3eb6a1aeac65
tsvetkovpro <285019@gmail.com>
2016-08-08 23:01:29 +0500

    add^
    font-awesome
    bootstrap
    jQuery
    
    create two rows on three goods

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Покупательская корзина' });
+  res.render('shop/index', { title: 'Покупательская корзина' });
 });
 
 module.exports = router;
