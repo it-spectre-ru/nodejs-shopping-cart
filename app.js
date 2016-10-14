e18a10508f2495ede10c56475c78a3b22d69ab7c
tsvetkovpro <285019@gmail.com>
2016-10-14 11:00:54 +0500

    Cart Model

--- app.js
+++ app.js
@@ -28,35 +28,36 @@ app.set('view engine', '.hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(validator());
 app.use(cookieParser());
 app.use(session({
-  secret: 'mysupersecret',
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  cookie: { maxAge: 180 * 60 * 1000 }
+	secret: 'mysupersecret',
+	resave: false,
+	saveUninitialized: false,
+	store: new MongoStore({mongooseConnection: mongoose.connection}),
+	cookie: {maxAge: 180 * 60 * 1000}
 }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-    res.locals.login = req.isAuthenticated();
-    res.locals.session = req.session;
-    next();
+app.use(function (req, res, next) {
+	res.locals.login = req.isAuthenticated();
+	/*global variable ---> view header.hbs */
+	res.locals.session = req.session;
+	next();
 });
 
 app.use('/user', userRoutes);
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -64,23 +65,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    Product.find(function (err, docs) {
-        var productChunks = [];
-        var chunkSize = 3;
-        for (var i = 0; i < docs.length; i += chunkSize) {
-            productChunks.push(docs.slice(i, i + chunkSize));
-        }
-        res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
-    });
+	Product.find(function (err, docs) {
+		var productChunks = [];
+		var chunkSize = 3;
+		for (var i = 0; i < docs.length; i += chunkSize) {
+			productChunks.push(docs.slice(i, i + chunkSize));
+		}
+		res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
+	});
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+	var productId = req.params.id;
+	var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+	Product.findById(productId, function (err, product) {
+		if (err) {
+			return res.redirect('/');
+			<!--todo Возможно стоит вывести сообщение об ошибке-->
+		}
+		cart.add(product, product.id);
+		req.session.cart = cart;
+		console.log(req.session.cart);
+		res.redirect('/');
+	});
 });
 
 module.exports = router;
